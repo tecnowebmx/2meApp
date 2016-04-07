1c1d4bad35515d69fa7fd7310922bd87e4c90776
Gustavo Reyes <gustavo.reyes@aplimovil.mx>
2016-04-07 12:15:30 -0500

    Cambie las funciones a personalizarlas a la funcion de controladores

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
+.controller('ChatsCtrl', function($scope, Categories) {
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
@@ -11,14 +11,14 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
+  $scope.categories = Categories.all();
+  $scope.remove = function(category) {
+    Categories.remove(category);
   };
 })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
+.controller('ChatDetailCtrl', function($scope, $stateParams, Categories) {
+  $scope.category = Categories.get($stateParams.categoryId);
 })
 
 .controller('ZoneCtrl', function($scope) {
