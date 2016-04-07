f5bf67626c31451518886597a6fe435c63a388ba
Gustavo Reyes <gustavo.reyes@aplimovil.mx>
2016-04-07 12:16:07 -0500

    Cambie la reerencia hacia chats a las categorías

--- www/js/services.js
+++ www/js/services.js
@@ -1,47 +1,67 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Categories', function() {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
-  var chats = [{
+  var categories = [{
     id: 0,
-    name: 'Ben Sparrow',
+    name: 'Todo para tu Viaje',
     lastText: 'You on your way?',
     face: 'img/ben.png'
   }, {
     id: 1,
-    name: 'Max Lynx',
+    name: 'Educativo',
     lastText: 'Hey, it\'s me',
     face: 'img/max.png'
   }, {
     id: 2,
-    name: 'Adam Bradleyson',
+    name: 'Hogar',
     lastText: 'I should buy a boat',
     face: 'img/adam.jpg'
   }, {
     id: 3,
-    name: 'Perry Governor',
+    name: 'Foto y Video',
     lastText: 'Look at my mukluks!',
     face: 'img/perry.png'
   }, {
     id: 4,
-    name: 'Mike Harrington',
+    name: 'Muebles, Ropa',
     lastText: 'This is wicked good ice cream.',
     face: 'img/mike.png'
+  }, {
+    id: 6,
+    name: 'Salud & Belleza',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 7,
+    name: 'Todo para tu Evento',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 8,
+    name: 'Bebés',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 9,
+    name: 'Moda',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
   }];
 
   return {
     all: function() {
-      return chats;
+      return categories;
     },
     remove: function(chat) {
       chats.splice(chats.indexOf(chat), 1);
     },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+    get: function(categoryId) {
+      for (var i = 0; i < categories.length; i++) {
+        if (categories[i].id === parseInt(categoryId)) {
+          return categories[i];
         }
       }
       return null;
