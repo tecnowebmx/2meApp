e4c651e59765ad4b131db125006d43d3d707344b
Gustavo Reyes <gustavo.reyes@aplimovil.mx>
2016-04-12 16:51:03 -0500

    Archivo para recibir información y envir por email

--- api/sendEmail.php
+++ api/sendEmail.php
@@ -1,20 +1,36 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
 $errors = [];
 $data = [];
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 
-if (empty($_POST['name']))
-  $errors['name'] = 'Nombre es requerido.';
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$comments = $_POST['comments'];
 
-if (empty($_POST['username']))
-  $errors['email'] = 'Email es required.';
+if (empty($name))
+  $errors[$name] = 'Nombre es requerido.';
+
+if (empty($email))
+  $errors[$email] = 'Email es required.';
 
 if (!empty($errors)) {
   $data['errors']  = $errors;
 } else {
-  $data['message'] = 'LA información esta correcta';
+  $data['message'] = 'Gracias por tu información';
 }
 
 echo json_encode($data);
+
+$message = "email: ".$email;
+$message = htmlspecialchars($comments);
+$message = stripslashes($comments);
+
+$to = "tano19@gmail.com";
+$subject = "Comentarios desde 2Me App";
+$header = "From:tano19@gmail.com";
+$enviar = mail($to,$subject,$message,$header);
 ?>
